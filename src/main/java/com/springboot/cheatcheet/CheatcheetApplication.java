e88f3c36d7b46df790dc65725034feb0d8d17e90
PyeonMinjun <alswns9810@gmail.com>
2024-03-08 23:04:10 +0900

    second commit

---
+++ src/main/java/com/springboot/cheatcheet/CheatcheetApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.cheatcheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheatcheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheatcheetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/cheatcheet/CheatcheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.cheatcheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheatcheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
